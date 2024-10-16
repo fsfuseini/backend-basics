2c0e6e7541bc361b59698ca3df9867b46a25bf93
fsfuseini <suhuyini86@gmail.com>
2024-10-16 14:51:37 +0000

    More Commit

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,8 +1,20 @@
+import { request } from "express";
 import { TodoModel } from "../models/todo.js";
+import { addTodoValidator, updateTodoValidator } from "../validators/todo.js";
 
 export const addTodo = async (req, res) => {
   try {
-    await TodoModel.create(req.body);
+    // Check if User is authenticated
+    // Validate user input
+    const { error, value } = addTodoValidator.validate({
+      ...req.body, icon: req.file?.filename
+    });
+    if (error) {
+      return res.status(422).json(error);
+    }
+
+    // Save Todo in the database
+    await TodoModel.create(value);
     res.status(201).json("Todo Added");
   } catch (error) {
     next(error);
@@ -10,9 +22,15 @@ export const addTodo = async (req, res) => {
 };
 
 export const getTodos = async (req, res, next) => {
-    try {
+  try {
+      const {filter = "{}", limit = 10, skip = 0} = req.query;
       // Fetch todos from the database
-    const todos = await TodoModel.find();
+    const todos = await TodoModel
+      .find(JSON.parse(filter))
+      .limit(limit)
+      .skip(skip);
+    
+    // Send response
     res.status(200).json(todos);
   } catch (error) {
     next(error);
--- controllers/user.js
+++ controllers/user.js
@@ -1,40 +1,69 @@
-import { registerUserValidator } from "../validators/user.js"; // Import validator
+import {
+    registerUserValidator,
+    loginUserValidator,
+} from "../validators/user.js"; // Import validator
 import { UserModel } from "../models/user.js"; // Import model
 import bcrypt from "bcryptjs"; // Import bcrypt
-
+import jwt from "jsonwebtoken"; // Import jwt
 
 export const registerUser = async (req, res, next) => {
-  try {
-    // Validate user input
-    const { error, value } = registerUserValidator.validate(req.body);
-    if (error) {
-      return res.status(422).json(error);
+    try {
+        // Validate user input
+        const { error, value } = registerUserValidator.validate(req.body);
+        if (error) {
+            return res.status(422).json(error);
+        }
+        // Check if user already exists
+        const user = await UserModel.findOne({ email: value.email });
+        if (user) {
+            return res.status(409).json("User already exists");
+        }
+        // Hash password
+        const hashedPassword = bcrypt.hashSync(value.password, 10);
+        // Save user
+        await UserModel.create({ ...value, password: hashedPassword });
+        // Send user confirmation email
+        // Respond to request with success message
+        res.json("User Registered");
+    } catch (error) {
+        next(error);
     }
-    // Check if user already exists
-    const user = await UserModel.findOne({ email: value.email });
-    if (user) {
-      return res.status(409).json("User already exists");
-    }
-    // Hash password
-    const hashedPassword = bcrypt.hashSync(value.password, 10);
-    // Save user
-    await UserModel.create({ ...value, password: hashedPassword });
-    // Send user confirmation email
-    // Respond to request with success message
-    res.json("User Registered");
-  } catch (error) {
-    next(error);
-  }
 };
 
-export const loginUser = (req, res, next) => {
-  res.json("User Logged In");
+export const loginUser = async (req, res, next) => {
+    try {
+        //  Validate user input
+        const { error, value } = loginUserValidator.validate(req.body);
+        if (error) {
+            return res.status(422).json(error);
+        }
+        // Find one user with identifier
+        const user = await UserModel.findOne({ email: value.email });
+        if (!user) {
+            return res.status(404).json("User not found");
+        }
+        // Compare password
+        const correctPassword = bcrypt.compareSync(value.password, user.password);
+        if (!correctPassword) {
+            return res.status(401).json("Incorrect Credentials");
+        }
+        // Sign a token for the user
+        const token = jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "24h" }
+        )
+        // Respond to request with success message
+        res.json({ message: "User Logged In", accessToken: token });
+    } catch (error) {
+        next(error);
+    }
 };
 
 export const logoutUser = (req, res, next) => {
-  res.json("User Logged Out");
+    res.json("User Logged Out");
 };
 
 export const updateProfile = (req, res, next) => {
-  res.json("Profile Updated");
+    res.json("Profile Updated");
 };
